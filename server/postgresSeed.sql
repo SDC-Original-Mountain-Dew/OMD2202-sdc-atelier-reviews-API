e5cf5939e8ce4bd119ce68e2d2799d7dcd54f571
Alex Shiao <alexshiao99@gmail.com>
2022-04-26 17:01:17 -0700

    Updated packages

--- server/postgresSeed.sql
+++ server/postgresSeed.sql
@@ -1,9 +1,5 @@
 \c sdc;
 --\! pwd;
-\copy Reviews FROM './reviews.csv' WITH (FORMAT CSV, HEADER);
-
-\copy Photos FROM './reviews_photos.csv' WITH (FORMAT CSV, HEADER);
-
 \copy Chars FROM './characteristics.csv' WITH (FORMAT CSV, HEADER);
 
 \copy CharacteristicsReviews FROM './characteristic_reviews.csv' WITH (FORMAT CSV, HEADER);
@@ -19,11 +15,22 @@ FROM Chars INNER JOIN
 ON Chars.id = filled_chars.characteristic_id
 ORDER BY filled_chars.characteristic_id;
 
+DROP TABLE CharacteristicsReviews;
+DROP TABLE Chars;
+
+
+\copy Reviews FROM './reviews.csv' WITH (FORMAT CSV, HEADER);
+
+\copy Photos FROM './reviews_photos.csv' WITH (FORMAT CSV, HEADER);
+
 INSERT INTO reviews2 (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness, photos)
 SELECT reviews.product_id, reviews.rating, reviews.date, reviews.summary, reviews.body, reviews.recommend, reviews.reported, reviews.reviewer_name,
   reviews.reviewer_email, reviews.response, reviews.helpfulness, COALESCE(photourl.photos, '[]') photos FROM reviews
 LEFT JOIN (SELECT review_id, JSON_AGG(JSON_BUILD_OBJECT('url', url)) photos FROM photos GROUP BY review_id) photourl
 ON photourl.review_id = reviews.id;
+
+DROP TABLE Photos;
+DROP TABLE reviews;
 
 UPDATE reviews2 SET date=date/1000;
 ALTER TABLE reviews2 ALTER date TYPE TIMESTAMP WITHOUT TIME ZONE USING to_timestamp(date) AT TIME ZONE 'UTC';
@@ -31,7 +38,3 @@ ALTER TABLE reviews2 ALTER date TYPE TIMESTAMP WITHOUT TIME ZONE USING to_timest
 CREATE INDEX char_char_idx ON characteristics(characteristic_id);
 CREATE INDEX char_prod_idx ON characteristics(product_id);
 CREATE INDEX reviews2_prod_idx ON reviews2(product_id);
-DROP TABLE CharacteristicsReviews;
-DROP TABLE Photos;
-DROP TABLE Chars;
-DROP TABLE reviews;
