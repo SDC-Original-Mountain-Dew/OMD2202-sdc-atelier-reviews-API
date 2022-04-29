24e1bbdbc9b92e38f4c7f92d21376ddd2e6faf0d
Alex Shiao <alexshiao99@gmail.com>
2022-04-29 09:25:11 -0700

    Added new relic and updated express server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
+require('newrelic');
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
 const Routers = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
+
 app.use('/reviews', Routers);
 
 app.listen(PORT, () => {
